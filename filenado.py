4369d043d875ccee147cc59585c770d5d6ce9954
CaryAndo <caryando@gmail.com>
2014-11-08 16:47:48 -0800

    Clean up some bad practices

--- filenado.py
+++ filenado.py
@@ -4,49 +4,51 @@ import sys
 import os
 import re
 
-def sort_dir(dire):
-	"""
-		Given a directory (string), sort all the files
-		into folders based on their extension
-	"""
-	directory = str(dire)
-	if os.path.isdir(directory):
-		for file in os.listdir(directory):
-			if os.path.isdir(file):
-				continue
-			if file[0] == '.':
-				print('Hidden file: ' + file + ' skipping')
-				continue
-			if file == __file__.replace('./', ''):
-				continue
 
-			splitted = re.split(r'\.\s*', file)
-			if len(splitted) == 1:
-				print('No file extension, skipping ' + file)
-				continue
-			extension = splitted[-1]
-			if os.path.isdir(directory + '/' + extension):
-				print("apparently " + directory + '/' + extension + ' is a folder!')
-				try:
-					print("tring to move to " + directory + '/' + extension + '/' + file)
-					os.rename(directory + '/' + file, directory + '/' + extension + '/' + file)
-					print('moved ' + file + ' to ' + '.' + extension + '/' + file)
-				except OSError:
-					print('Failed to move file: ' + file + '!')
-			else:
-				os.mkdir(directory + '/' + extension)
-				print("created " + directory + '/' + extension)
-				try:
-					os.rename(directory + '/' + file, './'+extension+'/'+file)
-				except OSError:
-					print('Failed to move file: ' + directory + '!')
-	elif os.path.isfile(directory):
-		print(directory + ' is not a directory!')
-	else:
-		print(directory + ' does not exist.')
+def sort_dir(dire):
+    """
+        Given a directory (string), sort all the single_files
+        into folders based on their extension
+    """
+    directory = str(dire)
+    if os.path.isdir(directory):
+        for single_file in os.listdir(directory):
+            if os.path.isdir(single_file):
+                continue
+            if single_file[0] == '.':
+                print('Hidden single_file: ' + single_file + ' skipping')
+                continue
+            if single_file == single_file.replace('./', ''):
+                continue
+
+            splitted = re.split(r'\.\s*', single_file)
+            if len(splitted) == 1:
+                print('No single_file extension, skipping ' + single_file)
+                continue
+            extension = splitted[-1]
+            if os.path.isdir(directory + '/' + extension):
+                print("apparently " + directory + '/' + extension + ' is a folder!')
+                try:
+                    print("tring to move to " + directory + '/' + extension + '/' + single_file)
+                    os.rename(directory + '/' + single_file, directory + '/' + extension + '/' + single_file)
+                    print('moved ' + single_file + ' to ' + '.' + extension + '/' + single_file)
+                except OSError:
+                    print('Failed to move single_file: ' + single_file + '!')
+            else:
+                os.mkdir(directory + '/' + extension)
+                print("created " + directory + '/' + extension)
+                try:
+                    os.rename(directory + '/' + single_file, './' + extension + '/' + single_file)
+                except OSError:
+                    print('Failed to move single_file: ' + directory + '!')
+    elif os.path.isfile(directory):
+        print(directory + ' is not a directory!')
+    else:
+        print(directory + ' does not exist.')
+
 
 if __name__ == '__main__':
-	for arg in sys.argv:
-		if arg == sys.argv[0]:
-			continue
-		sort_dir(arg)
+    for arg in sys.argv:
+        if arg == sys.argv[0]:
+            continue
+        sort_dir(arg)
